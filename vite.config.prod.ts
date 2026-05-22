1786e25ceec35e3a2a6684ff362479880c57aed5
campusalmenara-ui <campus.almenara@unimontes.br>
2026-05-22 17:17:48 -0300

    Create vite.config.prod.ts

---
+++ vite.config.prod.ts
@@ -0,0 +1,21 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: '/siteunimontes/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "src"),
+      "@shared": path.resolve(import.meta.dirname, "shared"),
+      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+    },
+  },
+  root: path.resolve(import.meta.dirname),
+  build: {
+    outDir: path.resolve(import.meta.dirname, "docs"),
+    emptyOutDir: true,
+  },
+});
